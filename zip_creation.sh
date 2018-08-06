ddbc16d71573644619e6b7cc7a46448fe1cf3451
stolz1982 <stolz1982@gmx.de>
2018-08-06 13:51:58 +0200

    edited

--- zip_creation.sh
+++ zip_creation.sh
@@ -1,8 +1,9 @@
 #/bin/bash
-ORIG_FILE="./250kb.file"
+ID="KE0903"
+ORIG_FILE="/home/administrator/zip_creation/250kb.file"
 DEST="/etc/odoo/import_export/export"
 DATE=`date '+%Y%m%d_%H%M%S'`
-DEST_FILE="$DEST/$DATE.zip"
+DEST_FILE="$DEST/$DATE-$ID.zip"
 
 cp $ORIG_FILE $DEST_FILE
 chown odoo:odoo $DEST_FILE
