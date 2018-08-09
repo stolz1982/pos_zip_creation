66224a904504cee56329e1573ef04caa6f9ce21c
stolz1982 <stolz1982@gmx.de>
2018-08-09 08:44:56 +0200

    Update and rename zip_creation.sh to pos_zip_creation.sh

--- pos_zip_creation.sh
+++ pos_zip_creation.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 ID="KE0903"
-ORIG_FILE="/home/administrator/zip_creation/250kb.file"
+ORIG_FILE="/home/administrator/pos_zip_creation/250kb.file"
 DEST="/etc/odoo/import_export/export"
 DATE=`date '+%Y%m%d_%H%M%S'`
 DEST_FILE="$DEST/$DATE-$ID.zip"
